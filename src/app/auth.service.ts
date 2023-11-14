36911d1187eb2156e7191731b6975a1fb95c50d4
Diego Seminario <practicas.0062@sesitdigital.com>
2023-11-13 20:06:00 -0500

    prototipo login variables locales

---
+++ src/app/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  users: any[] = [
+    { id: 1, name: 'admin', email: 'admin@gmail.com', password: '123' },
+    { id: 2, name: 'user', email: 'user@gmail.com', password: '123' },
+  ];
+  session: any;
+  constructor() {}
+
+  login(email:string,password:string){
+    let user = this.users.find((u)=>u.email === email && u.password === password)
+    if(user){
+      this.session = user;
+      localStorage.setItem('session',JSON.stringify(this.session));
+    }
+
+    return user;
+  }
+}

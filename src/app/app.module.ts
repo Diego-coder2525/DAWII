3dcbb44b00c4b860a6a1050586e76ccb6ab85ceb
FacuUL <facundodioses05@gmail.com>
2023-11-25 01:15:15 -0500

    q

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/seccion/header/header.component';
-import { OfertasComponent } from './components/seccion/ofertas/ofertas.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/seccion/login/login.component';
 import { RegistroComponent } from './components/seccion/registro/registro.component';
